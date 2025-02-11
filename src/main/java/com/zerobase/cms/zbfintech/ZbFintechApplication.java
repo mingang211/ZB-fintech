5335680715b66963f791aea2e0eee4b82eb2d882
mingang211 <a989977@naver.com>
2025-02-11 18:44:47 +0900

    Initial commit

---
+++ src/main/java/com/zerobase/cms/zbfintech/ZbFintechApplication.java
@@ -0,0 +1,13 @@
+package com.zerobase.cms.zbfintech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZbFintechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZbFintechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zerobase/cms/zbfintech/ZbFintechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerobase.cms.zbfintech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZbFintechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
