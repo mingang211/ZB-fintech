32e67cb30fbc0bd21b3de0fed37de8eab8132378
mingang211 <a989977@naver.com>
2025-03-07 21:33:38 +0900

    #2 test:002-User-Login 로그인 기능 테스트-> JwtAuthenticationProvider, UserControllerTest, UserLogInController에 대한 테스트 작성 - 실패 test는 시간이 너무 오래 걸릴 것 같아 보류 refact : Loginform에 어노테이션 추가

--- src/main/java/com/zerobase/zbfintech/dto/LoginForm.java
+++ src/main/java/com/zerobase/zbfintech/dto/LoginForm.java
@@ -1,8 +1,14 @@
 package com.zerobase.zbfintech.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoginForm {
     private String email;
     private String password;
